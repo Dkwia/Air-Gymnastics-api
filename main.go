d7a9cac0654faec0f68d1dade4f3d45c6948e72a
dkwia <bo@DESKTOP-BBPKBJ7.>
2025-05-02 00:47:15 +0700

    add service initialisation

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"context"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"go-gin-api/routes"
+	"go-gin-api/services"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	whatsAppService, err := services.NewWhatsAppService()
+	if err != nil {
+		log.Fatalf("Failed to initialize WhatsApp service: %v", err)
+	}
+	defer whatsAppService.Disconnect()
+
 	r := gin.Default()
 
 	routes.SetupRoutes(r)
 
-	if err := r.Run(":8080"); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
-	}
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		if err := r.Run(":8080"); err != nil {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+	}()
+
+	<-ctx.Done()
+	log.Println("Shutting down gracefully...")
 }
